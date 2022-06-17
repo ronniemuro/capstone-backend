3d39486fe808ce0a10d8e2e9dce35cb3d56a72d9
Ronnie Lopez-Muro <ronniethan@outlook.com>
2022-06-17 20:56:54 +0200

    created relationship model with associations with user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,9 @@ class User < ApplicationRecord
   has_many :posts
   # has_many :likes
   # has_many :comments
+  has_many :leader_relationships, class_name: "Relationship", foreign_key: "follower_id"
+  has_many :leaders, through: :leader_relationships, source: :leader
+
+  has_many :follower_relationships, class_name: "Relationship", foreign_key: "leader_id"
+  has_many :followers, through: :follower_relationships, source: :follower
 end
