777b00a1c5bf9c1ae06ffc3da617f167e527bcff
Ronnie Lopez-Muro <ronniethan@outlook.com>
2022-06-17 20:06:07 +0200

    added show action with http request

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,4 +3,9 @@ class PostsController < ApplicationController
     posts = Post.all
     render json: posts.as_json
   end
+
+  def show
+    post = Post.find_by(id: params["id"])
+    render json: post.as_json
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,5 @@ Rails.application.routes.draw do
 
   # Posts Routes
   get "/posts" => "posts#index"
+  get "/posts/:id" => "posts#show"
 end
