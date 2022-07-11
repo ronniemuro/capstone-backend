a2e44bd7ef40d7ba145b8535342bb40e76d01ebf
Ronnie Lopez-Muro <ronniethan@outlook.com>
2022-07-11 19:52:29 +0200

    adds cloudinary gem and code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   def index
     @users = User.all
+    #query params
     render template: "users/index"
   end
 
@@ -10,18 +11,28 @@ class UsersController < ApplicationController
   end
 
   def create
+    if params[:image_file]
+      response = Cloudinary::Uploader.upload(params[:image_file], {
+        cloud_name: Rails.application.credentials.cloudinary[:cloud_name],
+        api_key: Rails.application.credentials.cloudinary[:api_key],
+        api_secret: Rails.application.credentials.cloudinary[:api_secret],
+      })
+      cloudinary_url = response["secure_url"]
+    else
+      cloudinary_url = params[:profile_pic]
+    end
     user = User.new(
       name: params[:name],
       username: params[:username],
       email: params[:email],
       password: params[:password],
       password_confirmation: params[:password_confirmation],
-      profile_pic: params[:profile_pic],
+      profile_pic: cloudinary_url,
       sun: params[:sun],
       moon: params[:moon],
       rising: params[:rising],
     )
-    if user.save!
+    if user.save
       render json: { message: "User created successfully" }, status: :created
     else
       render json: { errors: user.errors.full_messages }, status: :bad_request
@@ -29,12 +40,19 @@ class UsersController < ApplicationController
   end
 
   def update
+    response = Cloudinary::Uploader.upload(params[:image_file], {
+      cloud_name: Rails.application.credentials.cloudinary[:cloud_name],
+      api_key: Rails.application.credentials.cloudinary[:api_key],
+      api_secret: Rails.application.credentials.cloudinary[:api_secret],
+    })
+    cloudinary_url = response["secure_url"]
+
     user_id = current_user.id
     user = User.find_by(id: user_id)
     user.name = params["name"] || user.name
     user.username = params["username"] || user.username
     user.email = params["email"] || user.email
-    user.profile_pic = params["profile_pic"] || user.profile_pic
+    user.profile_pic = cloudinary_url || user.profile_pic
     user.sun = params["sun"] || user.sun
     user.moon = params["moon"] || user.moon
     user.rising = params["rising"] || user.rising
