ae69fcdad6f9d374c8d4cc155f9821991cc2ff40
Ronnie Lopez-Muro <ronniethan@outlook.com>
2022-07-15 00:08:05 +0200

    final product for presentation

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -5,5 +5,4 @@ class Comment < ApplicationRecord
   validates :post_id, presence: true
   validates :user_id, presence: true
   validates :comment, presence: true
-  validates :comment, length: { in: 1..25 }
 end
