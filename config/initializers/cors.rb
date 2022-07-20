5152d4ef7eb98a25ae95270f0693e858c18d518f
Ronnie Lopez-Muro <ronniethan@outlook.com>
2022-07-20 18:54:44 +0200

    cors update

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "synastry.herokuapp.com", "*"
+    origins "https://synastry.herokuapp.com", "*"
 
     resource "*",
       headers: :any,
